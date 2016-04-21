59960aa134f7026604ea0095f6550f2bc628064e
hisenyuan <2811923@qq.com>
2016-04-21 15:41:11 +0800

    爬取百度百科100天数据<标题，摘要，链接>

--- src/baike_spider/spider_main.py
+++ src/baike_spider/spider_main.py
@@ -18,17 +18,25 @@ class SpiderMain(object):
     
     def craw(self, root_url):
         count = 1
+        # 把根链接添加进待爬链接列表
         self.urls.add_new_url(root_url)
+        # 判断是存在待爬取的链接
         while self.urls.has_new_url():
             try:
+                # 获取一个待爬取的链接
                 new_url = self.urls.get_new_url()
+                # 输出每次获取的链接
                 print 'craw %d : %s' % (count, new_url)
+                # 获取单个链接的网页内容
                 html_cont = self.downloader.download(new_url)
+                # 获取链接里面的url，以及标题和摘要
                 new_urls, new_data = self.parser.parse(new_url, html_cont)
+                # 把刚刚获取的url添加到待爬取的列表
                 self.urls.add_new_urls(new_urls)
+                # 把数据收集起来
                 self.outputer.collect_data(new_data)
-                
-                if count == 10:
+                # 控制循环次数
+                if count == 100:
                     break
                 count = count + 1
             except:
@@ -39,7 +47,7 @@ class SpiderMain(object):
     
 
 
-
+# 第一次写这个，main旁边少写了下划线，检查了好久~
 if __name__ == "__main__":
     root_url = "http://baike.baidu.com/view/21087.htm"
     obj_spider = SpiderMain()
